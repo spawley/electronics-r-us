a1ae45ef4e90890f9fc9118bef0e0ae2cf6d9ea8
Scott Pawley <spawley11@gmail.com>
2016-11-23 14:47:52 -0600

    province association with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122161021) do
+ActiveRecord::Schema.define(version: 20161123201141) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 20161122161021) do
     t.datetime "updated_at",                          null: false
     t.text     "name"
     t.text     "address"
+    t.integer  "province_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
